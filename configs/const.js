ed865bcbd48cceda59ca7241e53b0ef04fd902c4
hasharas <hasharamsankalpam@gmail.com>
2025-04-22 00:25:49 +0530

    Start:create config filr add base url and test urls

---
+++ configs/const.js
@@ -0,0 +1,11 @@
+//thare add to base url and othr urls
+
+export const URL = {
+
+      //we can add base url like this
+      BASE_URL: 'https://test-base-url.com',
+
+      //and allso we can add other urls like this
+      TEST_URL: 'https://test-url.com',
+      TEST_URL_2: 'https://test-url-2.com',
+}
